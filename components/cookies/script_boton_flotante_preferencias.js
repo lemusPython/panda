19dca7b9a032c73ee5452a0902632e0a3e7be24c
lemusPython <kevin.caballero2412@gmail.com>
2026-07-01 11:28:44 +0200

    nuevo

--- components/cookies/script_boton_flotante_preferencias.js
+++ components/cookies/script_boton_flotante_preferencias.js
@@ -1,14 +1,7 @@
 // Añadir un botón flotante para cambiar preferencias después de aceptar
 const floatingBtn = document.createElement('button');
-floatingBtn.innerHTML = `
-  <img src="https://media.istockphoto.com/id/466087002/es/foto/panda-rojo-del-%C3%A1rbol.jpg"
-       style="
-         width: 100%;
-         height: 100%;
-         border-radius: 50%;
-         object-fit: cover;
-       ">
-`;
+floatingBtn.innerHTML = '<img src="https://media.istockphoto.com/id/466087002/es/foto/panda-rojo-del-%C3%A1rbol.jpg?b=1&s=612x612&w=0&k=20&c=eK8G9zPMv_E6_SJbS5zhhp5dsPL6KTj6guCo-1EN9g4=">';
+floatingBtn.style.cssText = `
     position: fixed;
     bottom: 20px;
     left: 20px;
